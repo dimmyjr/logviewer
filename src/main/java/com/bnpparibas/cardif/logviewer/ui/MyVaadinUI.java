2aec89e519f81af0aa399f255122b4edd85761e1
Diego Schmidt <dceschmidt@gmail.com>
2014-10-03 18:11:55 -0300

    Adding ui example.

--- src/main/java/com/bnpparibas/cardif/logviewer/ui/MyVaadinUI.java
+++ src/main/java/com/bnpparibas/cardif/logviewer/ui/MyVaadinUI.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2014 Cardif.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of Cardif
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with Cardif.
- */
-
 package com.bnpparibas.cardif.logviewer.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
