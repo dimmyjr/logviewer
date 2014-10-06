be89baa035bc95b1057aa3f4ddbc0c7640d2a91e
Diego Schmidt <dceschmidt@gmail.com>
2014-10-06 15:28:23 -0300

    Added file tree example.

--- src/main/java/com/br/schmidt/logviewer/ui/MainUI.java
+++ src/main/java/com/br/schmidt/logviewer/ui/MainUI.java
@@ -10,6 +10,8 @@ import org.vaadin.spring.i18n.I18N;
 import com.vaadin.annotations.Title;
 import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.UI;
 
@@ -26,12 +28,17 @@ public class MainUI extends UI {
 
 	@Override
 	protected void init(final VaadinRequest request) {
+		HorizontalLayout layout = new HorizontalLayout();
+		setContent(layout);
 		//setContent(new Label(i18n.get("label.hello")));
 
-		FilesystemContainer container = new FilesystemContainer(new File("."), "java", true);
+		FilesystemContainer container = new FilesystemContainer(new File("/DevTools-v6_x64/var/was7_profile/Dev/logs"),
+				"log", true);
 		Tree tree = new Tree("Files", container);
-		setContent(tree);
+		ListSelect listSelect = new ListSelect("Files", container);
 
+		layout.addComponent(tree);
+		layout.addComponent(listSelect);
 	}
 
 }
